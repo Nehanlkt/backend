4f98c2778e1de5dde142776d74c930b60ceef225
nehanlkt <nehanlkt@gmail.com>
2025-11-26 16:39:08 +0530

    new

---
+++ files.js
@@ -0,0 +1,29 @@
+const { error } = require("console")
+const fs=require("fs")
+console.log(fs)
+
+// console.log("start")
+
+
+// fs.writeFileSync("harry.txt","hiii")
+// console.log("end")
+console.log("start")
+
+// write file
+fs.writeFile("harry2.txt","hi hloo",()=>{
+    console.log("done")
+})
+console.log("end")
+
+// readfile
+
+fs.writeFile("harry2.txt","hiii",()=>{
+    console.log("done")
+    fs.readFile("harry2.txt",(error,data)=>{
+        console.log(error,data.toString())
+    })
+})
+fs.appendFile("harry.txt","oiii",(e,d)=>{
+    console.log(d)
+})
+console.log("end")
--- main.js
+++ main.js
@@ -6,12 +6,18 @@
 // import obj from "./mymodule.js"
 // console.log(obj)
 // default import can be called by any name
-import neha from "./mymodule.js"
-console.log(neha)
-
+// import neha from "./mymodule.js"
+// console.log(neha)
+// to run in html also
 // to run this type:module add this in package.json
 
-// const a = require("./mymodule2.js")
-// console.log(a)
+// (function(exports,require,module,__filename,__dirname){
+    // module can actually lives here
+// })
+const a = require("./mymodule2.js")
+console.log(a,__dirname,__filename)
 // a can be of any name
+// all this wrapped in function
+
+
 
