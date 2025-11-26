537a96663cf030deccb3dd669a069f5c8ae6bcf2
nehanlkt <nehanlkt@gmail.com>
2025-11-26 15:18:32 +0530

    new

---
+++ main.js
@@ -0,0 +1,10 @@
+// import http from "http"
+// import {a,b} from "./mymodule.js"
+// console.log(a,b)
+// /these is for named import
+
+// import obj from "./mymodule.js"
+// console.log(obj)
+// default import can be called by any name
+import neha from "./mymodule.js"
+console.log(neha)
---
+++ mymodule.js
@@ -0,0 +1,20 @@
+export const a=1
+export const b=2
+export const c=4
+export const d=3
+export const e=6
+// all these are named export
+
+
+// let obj;
+// export default obj={
+//     x:5,
+//     y:7
+// }
+// default export
+
+const obj={
+    x:5,
+    y:7
+}
+export default obj;
