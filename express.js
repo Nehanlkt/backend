0d601d56466707337c3642b27d120766b74e66d0
nehanlkt <nehanlkt@gmail.com>
2025-11-29 12:36:40 +0530

    new

--- express.js
+++ express.js
@@ -1,14 +1,47 @@
-import http from "node:http";
+const express=require('express')
+const app=express()
 
-const hostname = '127.0.0.1';
-const port = 4000;
 
-const server =http. createServer((req, res) => {
+// const hostname = '127.0.0.1';
+const port = 3000;
+app.get('/',(req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('<h>Hello mm </h>');
+    res.setHeader('Content-Type', 'text/html');
+    res.send('<h1>Hello mm</h1> ');
+});
+app.get('/about',(req, res) => {
+    res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    res.send('about us');
+});
+app.get('/contact',(req, res) => {
+    res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    res.send('contact us ');
+});
+app.get('/ans',(req, res) => {
+    res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    res.send('ans me ');
+});
+// app.get('/ans/intro-to-py',(req, res) => {
+//     res.statusCode = 200;
+//     // res.setHeader('Content-Type', 'text/html');
+//     res.send('ans me ');
+// });
+// app.get('/ans/intro-to-java',(req, res) => {
+//     res.statusCode = 200;
+//     // res.setHeader('Content-Type', 'text/html');
+//     res.send('ans me ');
+// });
+app.get('/ans/:slug/:second',(req, res) => {
+    res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    console.log(req.params)
+    res.send('ans me ${req.params.slug} and ${req.params.second');
+    
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port,  () => {
+    console.log(`app listen ${port}`);
 });
