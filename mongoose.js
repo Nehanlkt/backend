89d38edda6c59d9142d32c518324c1394abdfdae
nehanlkt <nehanlkt@gmail.com>
2025-12-06 11:55:05 +0530

    new

---
+++ mongoose.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import express from "express";
+
+// let a=await mongoose.connect("mongodb://localhost:27017/")
+
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+
+//     const todo = new Todo({  desc: "Description of this todo", isDone: false, days: Math.floor(Math.random() * 45 + 5* Math.random()) })
+//     todo.save() 
+//     res.send('Hello World!')
+// })
+
+// app.get('/a', async (req, res) => { 
+//     let todo = await Todo.findOne({})
+//     console.log(todo)
+//     res.json({title: todo.title, desc: todo.desc})
+// })
